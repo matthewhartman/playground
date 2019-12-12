889f834bcd59cf2ffc4a5ea6ec17c666173c641f
Matthew Hartman <matthew.hartman@open.edu.au>
2019-12-12 16:15:08 +1100

    Lazy load google fonts and website header CSS

--- loadstyles.js
+++ loadstyles.js
@@ -1,1 +1,5 @@
-document.querySelector('head').insertAdjacentHTML('afterbegin', '<link href="styles.css?version=1417" rel="stylesheet" />');
+document.querySelector('head').insertAdjacentHTML('afterbegin', ' \
+  <link href="styles.css?version=1417" rel="stylesheet" /> \
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,100,300,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet"/> \
+    <link href="https://cdn.open.edu.au/website-header/default.css" rel="stylesheet"/> \
+');
