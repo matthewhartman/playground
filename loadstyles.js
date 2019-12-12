1bf649479fb5a2b58592cb5c2b339f7b11a16331
Matthew Hartman <matthew.hartman@open.edu.au>
2019-12-12 16:15:51 +1100

    Scroll to top

--- loadstyles.js
+++ loadstyles.js
@@ -3,3 +3,4 @@ document.querySelector('head').insertAdjacentHTML('afterbegin', ' \
   <link href="https://fonts.googleapis.com/css?family=Lato:400,100,300,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet"/> \
     <link href="https://cdn.open.edu.au/website-header/default.css" rel="stylesheet"/> \
 ');
+window.scrollTop(0, 0);
